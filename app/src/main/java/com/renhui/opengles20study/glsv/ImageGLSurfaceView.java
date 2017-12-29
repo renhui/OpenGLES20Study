dfa90a1e7e7d2633294a57c52166782190943c84
renhui <1418757529@qq.com>
2017-12-29 15:22:22 +0800

    图片处理完成

--- app/src/main/java/com/renhui/opengles20study/glsv/ImageGLSurfaceView.java
+++ app/src/main/java/com/renhui/opengles20study/glsv/ImageGLSurfaceView.java
@@ -14,14 +14,11 @@ import java.io.IOException;
  */
 public class ImageGLSurfaceView extends BaseGLSurfaceView {
 
-    ImageRenderer imageRenderer;
-
-    ImageTransformRenderer imageTransformRenderer;
-
     public ImageGLSurfaceView(Context context) throws IOException {
         super(context);
 
         // setRenderer(new ImageRenderer(context));  // 展示图片渲染器
+
         setRenderer(new ImageTransformRenderer(context, ImageTransformRenderer.Filter.MAGN));  // 展示图片处理渲染器
 
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
