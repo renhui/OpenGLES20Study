02d088c9c56434eb8d5bb04ee2e37e2ba0211d72
renhui <1418757529@qq.com>
2018-01-10 10:53:07 +0800

    提交代码

--- app/src/main/java/com/renhui/opengles20study/camera/filter/OesFilter.java
+++ app/src/main/java/com/renhui/opengles20study/camera/filter/OesFilter.java
@@ -7,18 +7,17 @@ import java.util.Arrays;
 
 public class OesFilter extends BaseFilter {
 
-    String vertex = "attribute vec4 vPosition;\n" +
+    String vertexShaderCode = "attribute vec4 vPosition;\n" +
             "attribute vec2 vCoord;\n" +
             "uniform mat4 vMatrix;\n" +
             "uniform mat4 vCoordMatrix;\n" +
             "varying vec2 textureCoordinate;\n" +
-            "\n" +
             "void main(){\n" +
             "    gl_Position = vMatrix*vPosition;\n" +
             "    textureCoordinate = (vCoordMatrix*vec4(vCoord,0,1)).xy;\n" +
             "}";
 
-    String fragment = "#extension GL_OES_EGL_image_external : require\n" +
+    String fragmentShaderCode = "#extension GL_OES_EGL_image_external : require\n" +
             "precision mediump float;\n" +
             "varying vec2 textureCoordinate;\n" +
             "uniform samplerExternalOES vTexture;\n" +
@@ -35,7 +34,7 @@ public class OesFilter extends BaseFilter {
 
     @Override
     protected void onCreate() {
-        createProgram(vertex, fragment);
+        createProgram(vertexShaderCode, fragmentShaderCode);
         mHCoordMatrix = GLES20.glGetUniformLocation(mProgram, "vCoordMatrix");
     }
 
